1d9b3faf55377cb2b936a8e5b6b2ed2eb0b0707b
rodrigobastosv <rodrigobastosv@gmail.com>
2020-11-12 09:09:44 -0300

    carregando promoção

--- lib/page/home/home_controller.dart
+++ lib/page/home/home_controller.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_place_models/models/models.dart';
+import 'package:my_place_utils/my_place_utils.dart';
 
 class HomeController {
   final _promocoesRef = FirebaseFirestore.instance.collection('promocoes');
+  final _produtosRef = FirebaseFirestore.instance.collection('produtos');
   final _categoriasRef = FirebaseFirestore.instance.collection('categorias');
 
   Future<List<PromocaoModel>> getPromocoes() async {
@@ -18,5 +20,20 @@ class HomeController {
     return querySnapshot.docs
         .map((doc) => CategoriaModel.fromJson(doc.id, doc.data()))
         .toList();
+  }
+
+  Future<ProdutoModel> getProdutoPromocao(PromocaoModel promocao) async {
+    final querySnapshot = await _produtosRef
+        .where(
+          'nome',
+          isEqualTo: promocao.nomeProduto,
+        )
+        .get();
+    final doc = querySnapshot.docs.first;
+    final produto = ProdutoModel.fromJson(doc.id, doc.data());
+    final preco =
+        promocao.valorOriginalProduto * (1 - (promocao.desconto / 100));
+    produto.preco = PrecoUtils.precoToString(preco.toStringAsFixed(2));
+    return produto;
   }
 }
--- lib/page/home/widget/promocoes.dart
+++ lib/page/home/widget/promocoes.dart
@@ -1,13 +1,18 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:my_place/page/carrinho/carrinho_controller.dart';
+import 'package:my_place/page/home/home_controller.dart';
+import 'package:my_place/page/produtos_por_categoria/produto_page.dart';
 
 import 'package:my_place_models/models/models.dart';
+import 'package:provider/provider.dart';
 import '../../../widget/mp_title.dart';
 
 class Promocoes extends StatelessWidget {
   Promocoes(this.promocoes);
 
   final List<PromocaoModel> promocoes;
+  final _controller = HomeController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +31,48 @@ class Promocoes extends StatelessWidget {
               .map(
                 (promocao) => ClipRRect(
                   borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    promocao.urlImagem,
-                    fit: BoxFit.cover,
+                  child: GestureDetector(
+                    onTap: () async {
+                      final produtoPromocao =
+                          await _controller.getProdutoPromocao(promocao);
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => Provider.value(
+                            value: Provider.of<CarrinhoController>(context),
+                            child: ProdutoPage(produtoPromocao),
+                          ),
+                        ),
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        Image.network(
+                          promocao.urlImagem,
+                          fit: BoxFit.cover,
+                        ),
+                        Positioned(
+                          top: 6,
+                          right: 10,
+                          child: Container(
+                            height: 40,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.red[400],
+                            ),
+                            child: Center(
+                              child: Text(
+                                '${promocao.desconto.toStringAsFixed(0)} % OFF',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               )
